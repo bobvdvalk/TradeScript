1f3b54cfe6fc33bcefd95f6bb64e7672ccc40367
Bob van der Valk <info@mawoo.nl>
2016-03-03 16:38:55 +0100

    Hello World application

---
+++ src/main/java/nl/mawoo/migratejs/Main.java
@@ -0,0 +1,10 @@
+package nl.mawoo.migratejs;
+
+/**
+ * Created by Bob on 3-3-2016.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
