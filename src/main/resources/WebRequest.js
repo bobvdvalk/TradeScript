59239412174cbfc843fbb57f957e728405134914
Bob van der Valk <info@mawoo.nl>
2016-03-07 00:22:39 +0100

    Add webrequest to library. (Not working (yet))

---
+++ src/main/resources/WebRequest.js
@@ -0,0 +1,13 @@
+var MigrateJS = MigrateJS || {};
+
+MigrateJS.webRequest = {
+  getRequest: function(request) {
+      var webRequestClass = Java.type("nl.mawoo.migratejs.extend.WebRequest");
+      var webRequest = new webRequestClass();
+
+      return webRequest.getRequestAsString(request);
+  },
+  sayHello: function() {
+      print("Hey, how are you?");
+  }
+};
