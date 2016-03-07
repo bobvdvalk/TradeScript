70bba8045e57762121951be34425a914fe82c1d9
Bob van der Valk <info@mawoo.nl>
2016-03-07 10:35:45 +0100

    Add post request (Not tested)

--- src/main/java/nl/mawoo/migratejs/extend/WebRequest.java
+++ src/main/java/nl/mawoo/migratejs/extend/WebRequest.java
@@ -1,9 +1,11 @@
 package nl.mawoo.migratejs.extend;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -52,7 +54,7 @@ public class WebRequest {
                 new InputStreamReader(con.getInputStream()));
 
         String inputLine;
-        StringBuffer response = new StringBuffer();
+        StringBuilder response = new StringBuilder();
 
         while((inputLine = in.readLine()) != null) {
             response.append(inputLine);
@@ -63,6 +65,38 @@ public class WebRequest {
         return response.toString();
     }
 
+    public String postRequestAsString(String parameterString) {
+        try {
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            // Send post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(parameterString);
+            wr.flush();
+            wr.close();
+
+            responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
 
 
     /**
