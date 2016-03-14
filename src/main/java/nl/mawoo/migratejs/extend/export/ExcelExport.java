cfa15a3cac8a7baedf4a26d9fe068dce34d548b0
Bob van der Valk <info@mawoo.nl>
2016-03-14 12:08:00 +0100

    Setting up excel export & reactoring

--- src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
+++ src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
@@ -1,10 +1,29 @@
 package nl.mawoo.migratejs.extend.export;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+
 /**
- * This class is responible to export data to Excel.
+ * This class is responible to export data to Excel .
  *
  * @author Bob van der Valk
  */
 public class ExcelExport {
+    private FileOutputStream fileOut;
+    private XSSFWorkbook workbook;
+    private XSSFSheet spreadsheet;
 
+    public ExcelExport(String name) {
+        workbook = new XSSFWorkbook();
+        spreadsheet = workbook.createSheet(name);
+
+
+    }
+
+    public ExcelExport() {
+        workbook = new XSSFWorkbook();
+        spreadsheet = workbook.createSheet();
+    }
 }
