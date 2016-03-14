89601430594d1fa99b3d038af242d5945662d3a0
Bob van der Valk <info@mawoo.nl>
2016-03-14 23:59:52 +0100

    Added constructor and comments

--- src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
+++ src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
@@ -21,36 +21,53 @@ public class ExcelExport {
     private XSSFSheet sheet;
     private XSSFRow row;
 
-    public ExcelExport(String filename) {
-        try {
-            workbook = new XSSFWorkbook();
-            sheet = workbook.createSheet();
-
-            FileOutputStream fileOut = new FileOutputStream(new File(filename));
-
-            workbook.write(fileOut);
-            fileOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Create a excel sheet
+     * @param name sheet name
+     */
+    public ExcelExport(String name) {
+        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet(name);
     }
 
+    /**
+     * Create a excel sheet
+     * ** Without a sheet name**
+     */
+    public ExcelExport() {
+        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet();
+    }
+
+    /**
+     * Add row to excelsheet
+     * @param rownum row number of sheet
+     */
     public void addRow(int rownum) {
         row = sheet.createRow(rownum);
     }
 
+    /**
+     * Add cell with content to sheet
+     * @param input string of information you want to put in the excel sheet
+     * @param cellnum cell number you want to set for the sheet.
+     */
     public void createCell(String input, int cellnum) {
         Cell cell = row.createCell(cellnum);
         cell.setCellValue(input);
     }
 
+    /**
+     * Save the excelsheet to a file
+     * @param filename name you want to give to the document
+     */
     public void save(String filename) {
         try {
 
             FileOutputStream outputStream = new FileOutputStream(new File(filename));
             workbook.write(outputStream);
             outputStream.close();
-            
+
         } catch (IOException e) {
             throw new CantSaveFileException("Can\'t save the excel file", e);
         }
