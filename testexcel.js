5971b9845ca685753832fc332c3e119803ea1b29
Bob van der Valk <info@mawoo.nl>
2016-03-15 00:21:22 +0100

    Test script to create a excel file

--- testexcel.js
+++ testexcel.js
@@ -1,3 +1,17 @@
 use("exceloutput.js");
 
-var output = new ExcelOutput("workbook.xlsx");
+print("Creating a excelfile");
+
+var output = new ExcelOutput("testsheet");
+
+var rownum = 0;
+
+for(i = 0; i < 10; i++){
+    output.addRow(rownum);
+    rownum++;
+
+    output.createCell("Hello", 0);
+    output.createCell("World!", 1);
+}
+
+output.save("workbook.xlsx");
