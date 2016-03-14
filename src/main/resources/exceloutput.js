154df739626f39c9337fce21c693d8f1a806dfb9
Bob van der Valk <info@mawoo.nl>
2016-03-15 00:13:01 +0100

    Extended excel library

--- src/main/resources/exceloutput.js
+++ src/main/resources/exceloutput.js
@@ -1,8 +1,30 @@
 /**
  * Create a blank excel page
- * @param filename name you want to give to the file.
+ * @param sheetname name you want to give to the spreadsheet.
  */
-var ExcelOutput = function(filename) {
+var ExcelOutput = function(sheetname) {
   var excelClass = Java.type("nl.mawoo.migratejs.extend.export.ExcelExport");
-  this.output = new excelClass(filename);
+  this.excelExport = new excelClass(sheetname);
 };
+    /**
+     * Add row to the spreadsheet.
+     * @param rowNumber number of the row you want to use to add cells.
+     */
+    ExcelOutput.prototype.addRow = function(rowNumber) {
+        this.excelExport.addRow(rowNumber);
+    };
+    /**
+     * Create a cell into the spreadsheet.
+     * @param input content you want to give to the cell
+     * @param cellnum cell number on the row.
+     */
+    ExcelOutput.prototype.createCell = function(input, cellnum) {
+        this.excelExport.createCell(input, cellnum);
+    };
+    /**
+     * Save the excel sheet to your location
+     * @param filename name of the file you want to set.
+     */
+    ExcelOutput.prototype.save = function(filename) {
+        this.excelExport.save(filename);
+    };
