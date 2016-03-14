88ff2a01b85bd1cd9503a613cd482e382595ae52
Bob van der Valk <info@mawoo.nl>
2016-03-14 23:55:50 +0100

    Add cells and rows to excel sheet and save to file.

--- src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
+++ src/main/java/nl/mawoo/migratejs/extend/export/ExcelExport.java
@@ -1,5 +1,9 @@
 package nl.mawoo.migratejs.extend.export;
 
+import nl.mawoo.migratejs.exceptions.CantSaveFileException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
@@ -13,9 +17,15 @@ import java.io.IOException;
  */
 public class ExcelExport {
 
+    private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+    private XSSFRow row;
+
     public ExcelExport(String filename) {
         try {
-            XSSFWorkbook workbook = new XSSFWorkbook();
+            workbook = new XSSFWorkbook();
+            sheet = workbook.createSheet();
+
             FileOutputStream fileOut = new FileOutputStream(new File(filename));
 
             workbook.write(fileOut);
@@ -23,7 +33,26 @@ public class ExcelExport {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public void addRow(int rownum) {
+        row = sheet.createRow(rownum);
+    }
 
+    public void createCell(String input, int cellnum) {
+        Cell cell = row.createCell(cellnum);
+        cell.setCellValue(input);
+    }
+
+    public void save(String filename) {
+        try {
+
+            FileOutputStream outputStream = new FileOutputStream(new File(filename));
+            workbook.write(outputStream);
+            outputStream.close();
+            
+        } catch (IOException e) {
+            throw new CantSaveFileException("Can\'t save the excel file", e);
+        }
     }
 }
