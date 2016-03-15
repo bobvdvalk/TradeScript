12ce19584279f4b7411069e5dcaf6ddcd7522b82
Bob van der Valk <info@mawoo.nl>
2016-03-15 23:19:40 +0100

    Multiple constructors

--- src/main/java/nl/mawoo/migratejs/extend/dbconnector/MongoDBConnector.java
+++ src/main/java/nl/mawoo/migratejs/extend/dbconnector/MongoDBConnector.java
@@ -1,8 +1,12 @@
 package nl.mawoo.migratejs.extend.dbconnector;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+
+import java.util.Arrays;
 
 /**
  * This class is responsible for the connection to MongoDB
@@ -14,8 +18,35 @@ public class MongoDBConnector {
     private MongoDatabase db;
 
     /**
-     * Start a MongoDB client
-     * @param database collection you want to connect.
+     * Start a MongoDB client with custom host, port, username, password
+     * @param host Address of MongoDB
+     * @param port Port of MongoDB
+     * @param database Database you want to use
+     * @param username credentials
+     * @param password credentials
+     */
+    public MongoDBConnector(String host, int port, String database, String username, char[] password) {
+        MongoCredential credential = MongoCredential.createCredential(username, database, password);
+        MongoClient mongoClient = new MongoClient(new ServerAddress(host, port), Arrays.asList(credential));
+        db = mongoClient.getDatabase(database);
+    }
+
+    /**
+     * Start a MongoDB client with credentials and default port and host
+     * @param database Database you want to use
+     * @param username credentials
+     * @param password credentials
+     */
+    public MongoDBConnector(String database, String username, char[] password) {
+        MongoCredential credential = MongoCredential.createCredential(username, database, password);
+        MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+
+        db = mongoClient.getDatabase(database);
+    }
+
+    /**
+     * Start a MongoDB client with credentials and default port and host and no credentials
+     * @param database Database you want to use
      */
     public MongoDBConnector(String database) {
         MongoClient mongoClient = new MongoClient();
