9d47d325acccb44918ec498b466e9c7b428b89f6
Bob van der Valk <info@mawoo.nl>
2016-03-15 10:57:26 +0100

    Prepare MongoDB implementation

---
+++ src/main/java/nl/mawoo/migratejs/extend/dbconnector/MongoDBConnector.java
@@ -0,0 +1,13 @@
+package nl.mawoo.migratejs.extend.dbconnector;
+
+/**
+ * This class is responsible for the connection to MongoDB
+ *
+ * @author Bob van der Valk
+ */
+public class MongoDBConnector {
+
+    public MongoDBConnector() {
+        //TODO: this funciton
+    }
+}
