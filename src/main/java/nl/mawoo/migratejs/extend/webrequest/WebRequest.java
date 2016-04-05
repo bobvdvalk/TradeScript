78acaede8e499dc866bfb9ef4cef0702441636fa
Bob van der Valk <info@mawoo.nl>
2016-04-05 13:26:21 +0200

    Add post request

--- src/main/java/nl/mawoo/migratejs/extend/webrequest/WebRequest.java
+++ src/main/java/nl/mawoo/migratejs/extend/webrequest/WebRequest.java
@@ -41,14 +41,27 @@ public class WebRequest {
 
     /**
      * GET web request using the apache library
-     * @param request
-     * @return
+     *
+     * NOTE: this method starts with a capital because end-users can use the libraries documentation.
+     * Documentation can be found at: https://hc.apache.org/httpcomponents-client-ga/tutorial/html/fluent.html
+     * @param request url you want to send a get request to
+     * @return return REQUEST object
      */
-    public static Request get(String request) {
+    public static Request Get(String request) {
         return Request.Get(request);
     }
 
-
+    /**
+     * POST webrequest using the apache library
+     *
+     * NOTE: this method starts with a capital because end-users can use the libraries documentation.
+     * Documentation can be found at: https://hc.apache.org/httpcomponents-client-ga/tutorial/html/fluent.html
+     * @param request url you want to send a get request to
+     * @return REQUEST object
+     */
+    public static Request Post(String request) {
+        return Request.Post(request);
+    }
 
     /**
      * Execute a get request and the data will return as String
