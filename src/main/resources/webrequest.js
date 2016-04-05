530c12a65b08ea1ce511dc5390c1eb7c84a63a75
Bob van der Valk <info@mawoo.nl>
2016-04-05 13:43:16 +0200

    Remove old code

--- src/main/resources/webrequest.js
+++ src/main/resources/webrequest.js
@@ -1,25 +1,3 @@
-WebRequest = {
-
-    /**
-     * This method is responsible for the get web request.
-     * @param request url you want to reach
-     * @returns string with content of the request.
-     */
-    getRequest: function(request) {
-      var webRequestClass = Java.type("nl.mawoo.migratejs.extend.webrequest.WebRequest");
-      var webRequest = new webRequestClass(request);
-
-      return webRequest.getRequestAsString();
-    },
-
-    postRequest: function(request, parameters) {
-        var webRequestClass = Java.type("nl.mawoo.migratejs.extend.webrequest.WebRequest");
-        var webRequest = new webRequestClass(request);
-
-        return webRequest.postRequestAsString(parameters);
-    }
-};
-
 Request = {
     /**
      * Send a GET request to a url.
