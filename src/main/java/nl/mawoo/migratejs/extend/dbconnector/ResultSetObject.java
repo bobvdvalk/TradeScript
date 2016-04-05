1e13d4d243b81cec4ee2f22b56b3a9937e5a970a
Bob van der Valk <info@mawoo.nl>
2016-04-05 10:37:49 +0200

    Changed return type of json String to objects you can pick.

--- src/main/java/nl/mawoo/migratejs/extend/dbconnector/ResultSetObject.java
+++ src/main/java/nl/mawoo/migratejs/extend/dbconnector/ResultSetObject.java
@@ -21,18 +21,63 @@ public class ResultSetObject {
     }
 
     /**
+     * Use this function to loop the ResultSet.
+     * @return the next row of colums
+     *
+     * @throws SQLException - when a sql error occurs
+     */
+    public boolean next() throws SQLException {
+        return rs.next();
+    }
+
+    /**
+     * Return every object from the ResultSet
+     * @param columnName name of the column you want to get data from
+     * @return object with your data
+     *
+     * @throws SQLException if a SQL error occurs
+     */
+    public Object get(String columnName) throws SQLException {
+        return rs.getObject(columnName);
+    }
+    /**
+     * Return every object from the ResultSet
+     * @param columnName id of the column you want to get data from
+     * @return object with your data
+     *
+     * @throws SQLException if a SQL error occurs
+     */
+    public Object get(int columnName) throws SQLException {
+        return rs.getObject(columnName);
+    }
+
+    /**
      * Get a String from a ResultSet
-     * @param columnName column you want to get information from
+     * @param columnName name you want to get information from
      * @return data as a string in the column
+     *
      * @throws SQLException If an error occurs in the database
      */
     public String getString(String columnName) throws SQLException {
         return rs.getString(columnName);
     }
+
     /**
      * Get a String from a ResultSet
      * @param columnName column you want to get information from
      * @return data as a string in the column
+     *
+     * @throws SQLException If an error occurs in the database
+     */
+    public String getString(int columnName) throws SQLException {
+        return rs.getString(columnName);
+    }
+
+    /**
+     * Get a String from a ResultSet
+     * @param columnName column you want to get information from
+     * @return data as a string in the column
+     *
      * @throws SQLException If an error occurs in the database
      */
     public int getInt(String columnName) throws SQLException {
@@ -43,9 +88,32 @@ public class ResultSetObject {
      * Get a String from a ResultSet
      * @param columnName column you want to get information from
      * @return data as a string in the column
+     *
+     * @throws SQLException If an error occurs in the database
+     */
+    public int getInt(int columnName) throws SQLException {
+        return rs.getInt(columnName);
+    }
+
+    /**
+     * Get a String from a ResultSet
+     * @param columnName column you want to get information from
+     * @return data as a string in the column
+     *
      * @throws SQLException If an error occurs in the database
      */
     public Date getDate(String columnName) throws SQLException {
+        return rs.getDate(columnName);
+    }
+
+    /**
+     * Get a String from a ResultSet
+     * @param columnName column you want to get information from
+     * @return data as a string in the column
+     *
+     * @throws SQLException If an error occurs in the database
+     */
+    public Date getDate(int columnName) throws SQLException {
         return rs.getDate(columnName);
     }
 }
