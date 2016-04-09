a0e32483327a41a9516417c6062550b96247522f
Bob van der Valk <info@mawoo.nl>
2016-04-09 15:46:12 +0200

    Remove test files - To see the files look at: https://github.com/mawoonl/WCMScript-Examples

--- mongotest.js
+++
@@ -1,9 +0,0 @@
-use("mongodb.js");
-
-print("start");
-
-var mongoConnector = MongoConnector("local");
-
-var input = "{name: \"Joshua Jenster\", company: \"Mawoo\"}";
-
-mongoConnector.getCollection("Test_collection").insertOne(input);
