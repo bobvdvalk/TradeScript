479ea63b0725d099362d30fe76a0aed270c793c4
Bob van der Valk <info@mawoo.nl>
2016-04-11 15:10:43 +0200

    Add crawler object

--- src/main/java/nl/mawoo/wcmscript/crawler/CrawlObject.java
+++ src/main/java/nl/mawoo/wcmscript/crawler/CrawlObject.java
@@ -1,7 +1,67 @@
 package nl.mawoo.wcmscript.crawler;
 
+import org.jsoup.nodes.Document;
+
 /**
- * Created by bobva on 11-04-16.
+ * This object is responsible to save any crawl data
+ *
+ * @author Bob van der Valk
  */
 public class CrawlObject {
+    private String url;
+    private String head;
+    private String title;
+    private String body;
+    private Document document;
+
+    public CrawlObject(String url, String head, String title, String body) {
+        this.url = url;
+        this.head = head;
+        this.title = title;
+        this.body = body;
+    }
+
+    public CrawlObject(Document document) {
+        this.document = document;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
 }
