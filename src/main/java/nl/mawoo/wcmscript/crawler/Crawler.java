c50b2637e115c4c7f70cd943e1844a6f8378d544
Bob van der Valk <info@mawoo.nl>
2016-04-11 16:32:01 +0200

    Return a crawled document

--- src/main/java/nl/mawoo/wcmscript/crawler/Crawler.java
+++ src/main/java/nl/mawoo/wcmscript/crawler/Crawler.java
@@ -3,11 +3,8 @@ package nl.mawoo.wcmscript.crawler;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * This class is responsible to crawl every website you want
@@ -16,11 +13,9 @@ import java.util.ArrayList;
  */
 public class Crawler {
     private Logger logger = Logger.getLogger(Crawler.class.getName());
-    private String element;
-    private ArrayList<Document> arrayList;
+    private Document document;
 
-    public Crawler(String element) {
-        this.element = element;
+    private Crawler() {
     }
 
     /**
@@ -30,22 +25,20 @@ public class Crawler {
      * @return ArrayList with CrawlObjects
      */
     public void run(String url) {
-        Document document = null;
+        document = null;
 
         try {
             document = Jsoup.connect(url).get();
         } catch (IOException e) {
             logger.error("Can\'t get contents of "+ url +": "+ e);
         }
-
-        //TODO: add to arraylist
-
-        Elements questions = document.select("a[href]");
-        for(Element link: questions){
-            if(link.attr("href").contains("mit.edu"))
-                this.run(link.attr("abs:href"));
-        }
     }
 
-
+    /**
+     * Get the current document
+     * @return craweld website.
+     */
+    public Document getDocument() {
+        return document;
+    }
 }
