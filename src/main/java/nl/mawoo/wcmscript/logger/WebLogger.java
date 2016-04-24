03e47eb3bd5b9064434213b45d62f6e016d2f273
Bob van der Valk <info@mawoo.nl>
2016-04-24 14:32:58 +0200

    Dunno yet..

--- src/main/java/nl/mawoo/wcmscript/logger/WebLogger.java
+++ src/main/java/nl/mawoo/wcmscript/logger/WebLogger.java
@@ -1,36 +1,44 @@
 package nl.mawoo.wcmscript.logger;
 
+import org.apache.log4j.Logger;
+
 /**
- * Created by bobva on 23-04-16.
+ * This class is responsible to log to the WebManager
+ * TODO: create stream to web
+ * @author Bob van der Valk
  */
 public class WebLogger implements WCMSLogger {
+
+    private Logger logger;
+
     @Override
     public WCMSLogger getLogger(Class clazz) {
-        return null;
+        logger = Logger.getLogger(clazz.getName());
+        return this;
     }
 
     @Override
     public void error(String message) {
-
+        logger.error(message);
     }
 
     @Override
     public void error(String message, Throwable cause) {
-
+        logger.error(message, cause);
     }
 
     @Override
     public void info(String message) {
-
+        logger.info(message);
     }
 
     @Override
     public void info(String message, Throwable cause) {
-
+        logger.info(message, cause);
     }
 
     @Override
     public void warning(String message) {
-
+        logger.warn(message);
     }
 }
