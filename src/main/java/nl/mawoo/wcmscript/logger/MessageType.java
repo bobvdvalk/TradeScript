2350277ca0a5ff54b84b03e4d529820bf9703d91
Bob van der Valk <info@mawoo.nl>
2016-04-26 11:30:18 +0200

    Add enum MessageType

---
+++ src/main/java/nl/mawoo/wcmscript/logger/MessageType.java
@@ -0,0 +1,12 @@
+package nl.mawoo.wcmscript.logger;
+
+/**
+ * Defines the message types for the logger.
+ *
+ * @author Bob van der Valk
+ */
+public enum MessageType {
+    INFO,
+    ERROR,
+    WARNING
+}
