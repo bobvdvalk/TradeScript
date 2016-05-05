457cf998d03af8ab4d87bc070c043b2b11323dff
Bob van der Valk <info@mawoo.nl>
2016-05-05 23:07:48 +0200

    Add comment. temporary disable the weblogger

--- src/main/java/nl/mawoo/wcmscript/logger/WCMSLogger.java
+++ src/main/java/nl/mawoo/wcmscript/logger/WCMSLogger.java
@@ -7,6 +7,10 @@ import java.util.Properties;
 
 /**
  * This class is repsonsible to log items to the console or to the web.
+ * In the case the WCMScript is connected to a "manager". The info messages, errors and warnings are stored in a
+ * mysql database. This is because on the "manager" there is a console reader what shows the output to the front-end.
+ * If WCMScript is not connected to a "manager" the logger just normally outputs to the console as a standard application
+ * will.
  *
  * @author Bob van der Valk
  */
@@ -19,13 +23,13 @@ public class WCMSLogger {
     public static AbstractLogger getLogger(Class clazz) {
 
         File f = new File("config.properties");
-        Properties prop = WCMSProperties.get();
-
-        if(prop.getProperty("web").equals("true")) {
-            return new WebLogger(prop.getProperty("session_id"));
-        } else {
+//        Properties prop = WCMSProperties.get();
+//
+//        if(prop.getProperty("web").equals("true")) {
+//            return new WebLogger(prop.getProperty("session_id"));
+//        } else {
             return new ConsoleLogger();
-        }
+//        }
     }
 
 }
