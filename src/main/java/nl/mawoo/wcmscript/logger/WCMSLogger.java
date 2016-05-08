00ed23ce54a47be880f504c7b7e5cf458e8cdc22
Bob van der Valk <info@mawoo.nl>
2016-05-08 15:18:14 +0200

    check if there is a session id present

--- src/main/java/nl/mawoo/wcmscript/logger/WCMSLogger.java
+++ src/main/java/nl/mawoo/wcmscript/logger/WCMSLogger.java
@@ -4,7 +4,7 @@ package nl.mawoo.wcmscript.logger;
 import nl.mawoo.wcmscript.scriptengine.ScriptHandler;
 
 /**
- * This class is repsonsible to log items to the console or to the web.
+ * This class is responsible to log items to the console or to the web.
  * In the case the WCMScript is connected to a "manager". The info messages, errors and warnings are stored in a
  * mysql database. This is because on the "manager" there is a console reader what shows the output to the front-end.
  * If WCMScript is not connected to a "manager" the logger just normally outputs to the console as a standard application
@@ -19,12 +19,11 @@ public class WCMSLogger {
     }
 
     public static AbstractLogger getLogger(Class clazz) {
-        System.out.println("The session id is: "+ ScriptHandler.getSessionId());
 
-        if(ScriptHandler.getSessionId().isEmpty()) {
+        if(!ScriptHandler.getSessionIdBinding().isEmpty()) {
             return new ConsoleLogger();
         } else {
-            return new WebLogger(ScriptHandler.getSessionId());
+            return new WebLogger(ScriptHandler.getSessionIdBinding());
         }
     }
 
