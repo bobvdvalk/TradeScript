017ee314502806f440a471f249f8deb6abafadd4
Bob van der Valk <info@mawoo.nl>
2016-05-10 23:19:35 +0200

    Add NashornManager class

---
+++ src/main/java/nl/mawoo/wcmscript/scriptengine/NashornManager.java
@@ -0,0 +1,18 @@
+package nl.mawoo.wcmscript.scriptengine;
+
+import javax.script.ScriptEngine;
+
+/**
+ * {message}
+ *
+ * @author Bob van der Valk
+ */
+public class NashornManager {
+    private ScriptEngine nashorn;
+
+    public NashornManager(ScriptEngine nashorn) {
+        this.nashorn = nashorn;
+    }
+
+    
+}
