026497b75343df925a871e0c3f79c0e2208dd7aa
Bob van der Valk <info@mawoo.nl>
2016-05-26 14:04:15 +0200

    Add license header & add setters

--- wcmscript-core/src/main/java/nl/mawoo/wcmscript/modules/mongodb/MongoDB.java
+++ wcmscript-core/src/main/java/nl/mawoo/wcmscript/modules/mongodb/MongoDB.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2016 Mawoo Nederland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package nl.mawoo.wcmscript.modules.mongodb;
 
 import com.mongodb.MongoClient;
@@ -16,11 +32,30 @@ public class MongoDB extends AbstractScriptModule {
     private String currentDatabase = null;
     private String host = "127.0.0.1";
     private int port = 27017;
-    private String username = null;
-    private String password = null;
 
     public MongoDB() {
 
+    }
+
+    /**
+     * Set a database that has to be used
+     * @param currentDatabase database you want to use
+     */
+    public void setDatabase(String currentDatabase) {
+        this.currentDatabase = currentDatabase;
+    }
+
+    /**
+     * Change default host
+     * default: 127.0.0.1
+     * @param host
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 
     /**
@@ -42,7 +77,7 @@ public class MongoDB extends AbstractScriptModule {
      * @return this
      */
     public MongoDB connectAuthenticated(String username, String password) {
-        MongoCredential mongoCredential = MongoCredential.createMongoCRCredential(this.username, this.currentDatabase, this.password.toCharArray());
+        MongoCredential mongoCredential = MongoCredential.createMongoCRCredential(username, this.currentDatabase, password.toCharArray());
         MongoClient client = new MongoClient(new ServerAddress(host, port), Arrays.asList(mongoCredential));
         client.getDatabase(currentDatabase);
         return this;
