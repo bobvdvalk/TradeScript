eb4645ee0bf61d2aaad3c62b9475165a4a505789
Bob van der Valk <info@mawoo.nl>
2016-06-18 15:19:40 +0200

    add getSheet and row iterator

--- wcmscript-core/src/main/java/nl/mawoo/wcmscript/modules/excel/ExcelImportV2.java
+++ wcmscript-core/src/main/java/nl/mawoo/wcmscript/modules/excel/ExcelImportV2.java
@@ -1,6 +1,8 @@
 package nl.mawoo.wcmscript.modules.excel;
 
 import nl.mawoo.wcmscript.AbstractScriptModule;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -8,6 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * This class is responsible to read/import excel files
@@ -29,7 +32,6 @@ public class ExcelImportV2 extends AbstractScriptModule {
             File file = new File(path);
             inputStream = new FileInputStream(file);
             workbook = new XSSFWorkbook(inputStream);
-
             return this;
         } catch (FileNotFoundException e) {
             getScriptLogger().error("Can\'t open the selected file in path");
@@ -40,4 +42,21 @@ public class ExcelImportV2 extends AbstractScriptModule {
         return null;
     }
 
+    /**
+     * This method is responsible to open the right sheet
+     * @param name the name of the sheet you want to read
+     * @return this for chaining, read rows, cells, etc
+     */
+    public ExcelImportV2 getSheet(String name) {
+        sheet = workbook.getSheet(name);
+        return this;
+    }
+
+    /**
+     * Returns for iterator so you can "scan" the excel sheet
+     * @return iterator of rows from the excel sheet
+     */
+    public Iterator<Row> rowIterator() {
+        return sheet.rowIterator();
+    }
 }
