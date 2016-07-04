d161fef2617647838dd22e64bda190a9a249c076
Bob van der Valk <info@mawoo.nl>
2016-07-04 16:21:15 +0200

    Add powerpoint module

---
+++ wcmscript-core/src/main/java/nl/mawoo/wcmscript/modules/powerpoint/PowerpointImporter.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2016 Mawoo Nederland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.mawoo.wcmscript.modules.powerpoint;
+
+import nl.mawoo.wcmscript.AbstractScriptModule;
+
+/**
+ * This class is responsible to import the contents of a powerpoint presentation
+ *
+ * @author Bob van der Valk
+ */
+public class PowerpointImporter extends AbstractScriptModule {
+
+    public void open(String path) {
+
+    }
+}
